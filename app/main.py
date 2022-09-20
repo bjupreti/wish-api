14c66427a617b9b5155588cbdb768bdda20e476b
bjupreti <bjupreti123@gmail.com>
2022-09-20 01:42:39 -0400

    added cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
-from .database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .routers import post, user, auth
 
-# Alembic takes care of creating the tables, so it's not needed.
-# Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
